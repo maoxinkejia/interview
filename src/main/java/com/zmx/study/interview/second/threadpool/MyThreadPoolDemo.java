38ae373cdb9f01549449a93f79efbd8060819d13
zhangmaoxin <497122067@qq.com>
2024-01-08 11:25:37 +0800

    demo

--- src/main/java/com/zmx/study/interview/second/threadpool/MyThreadPoolDemo.java
+++ src/main/java/com/zmx/study/interview/second/threadpool/MyThreadPoolDemo.java
@@ -1,43 +1,84 @@
 package com.zmx.study.interview.second.threadpool;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.*;
+
+@Slf4j
 public class MyThreadPoolDemo {
-    public static void main(String[] args) {
-        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(2,
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Set<Integer> set = new CopyOnWriteArraySet<>();
+
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+                2,
                 5,
-                1L,
+                600L,
                 TimeUnit.SECONDS,
-                new LinkedBlockingDeque<>(3),
+                new LinkedBlockingDeque<>(5),
                 Executors.defaultThreadFactory(),
                 new ThreadPoolExecutor.AbortPolicy());
 //                new ThreadPoolExecutor.CallerRunsPolicy());
 //                new ThreadPoolExecutor.DiscardOldestPolicy());
 //                new ThreadPoolExecutor.DiscardPolicy());
 
+
         try {
             for (int i = 1; i <= 10; i++) {
-                final int tmp = i;
-                threadPool.execute(() -> {
-                    System.out.println(Thread.currentThread().getName() + "\t进入了，准备执行：" + tmp);
-                    try {
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    System.out.println(Thread.currentThread().getName() + "\t 执行完成：" + tmp);
-                });
+                set.add(i);
+                threadPool.execute(new MyTask(i, set));
             }
         } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            threadPool.shutdown();
+            log.error("", e);
         }
 
 
+
+        while (!set.isEmpty()) {
+            log.info("flag={}", threadPool.isTerminated());
+            TimeUnit.SECONDS.sleep(1L);
+        }
+
+        log.info("shutdown threadPool...");
+        threadPool.shutdown();
+        log.info("finished");
+    }
+}
+
+
+@Slf4j
+class MyTask implements Runnable {
+
+    private final int tmp;
+    private final Set<Integer> set;
+
+
+    public MyTask(int tmp, Set<Integer> set) {
+        this.tmp = tmp;
+        this.set = set;
+    }
+
+    @Override
+    public void run() {
+        log.info("\t进入了，准备执行：" + tmp);
+
+        log.info("before set={}", Arrays.toString(set.toArray()));
+
+        try {
+            int sleep = new Random().nextInt(2) + 2;
+            log.info("sleep time={}s", sleep);
+
+            TimeUnit.SECONDS.sleep(sleep);
+
+            boolean removed = set.remove(tmp);
+            log.info("after set={}, removed={}, element={}", set, removed, tmp);
+        } catch (InterruptedException e) {
+            log.error("", e);
+        }
+        log.info("\t 执行完成：" + tmp);
     }
 }
